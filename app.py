699d9099c118914323de990202df90037276a9f3
GustavoSB3 <161391954+GustavoSB3@users.noreply.github.com>
2026-03-23 22:40:40 -0300

    mesclar html + app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from flask_cors import CORS
-from projeto1 import mesclar_pdfs as merger
+import PyPDF2
 from io import BytesIO
 
 app = Flask(__name__)
@@ -8,7 +8,23 @@ CORS(app)
 
 @app.route("/merger", methods=["POST"])
 def mesclar():
-    caminho = merger()
-    return send_file(caminho, as_attachment=True)
+    arquivos = request.files.getlist("pdfs")  # 🔥 pega os arquivos
+
+    merger = PyPDF2.PdfMerger()
+
+    for arquivo in arquivos:
+        merger.append(arquivo)
+
+    output = BytesIO()
+    merger.write(output)
+    merger.close()
+
+    output.seek(0)
+
+    return send_file(
+        output,
+        download_name="PDF_Final.pdf",
+        as_attachment=True
+    )
 
 app.run(host="0.0.0.0", port=10000)
