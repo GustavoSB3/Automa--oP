2b6e263156c7201d38a062db309db89fdbd3864f
Gustavo Berg <gustavo2berg@gmail.com>
2026-03-25 16:06:40 -0300

    add excel.py

---
+++ Excel/excel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+tabela = pd.read_excel("Excel\Planilha de Teste Logística e Desafios.xlsx")
+print(tabela)
+
+def engine_dados():{
+    host:
+    user:
+    password:
+    db:
+}
--- app.py
+++ app.py
@@ -1,30 +1,21 @@
-from flask import Flask, send_file, request
-from flask_cors import CORS
-import PyPDF2
-from io import BytesIO
+from flask import Flask, send_file, render_template
+from projeto1 import lista_arquivos
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/merger", methods=["POST"])
+@app.route("/")
+def home():
+   titulo = "Gestão de Usuários"
+   usuarios = [
+      { "nome": "Guilherme", "membro_ativo": True },
+      { "nome": "Guilherme", "membro_ativo": True },
+      { "nome": "Guilherme", "membro_ativo": True },
+      ]
+   return render_template("index.html", titulo=titulo, usuarios=usuarios)
+
+@app.route("/mesclar")
 def mesclar():
-    arquivos = request.files.getlist("pdfs")  # 🔥 pega os arquivos
+   caminho = lista_arquivos()
+   return send_file(caminho, as_attachment=True)
 
-    merger = PyPDF2.PdfMerger()
-
-    for arquivo in arquivos:
-        merger.append(arquivo)
-
-    output = BytesIO()
-    merger.write(output)
-    merger.close()
-
-    output.seek(0)
-
-    return send_file(
-        output,
-        download_name="PDF_Final.pdf",
-        as_attachment=True
-    )
-
-app.run(host="0.0.0.0", port=10000)
+app.run(debug=True)
