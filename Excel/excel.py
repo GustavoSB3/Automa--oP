ccd2eba11623527b8ffc4f10143e2e628391ff5a
GustavoSB3 <161391954+GustavoSB3@users.noreply.github.com>
2026-03-25 22:31:43 -0300

    add engine

--- Excel/excel.py
+++ Excel/excel.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 tabela = pd.read_excel("Excel\Planilha de Teste Logística e Desafios.xlsx")
 print(tabela)
 
-def engine_dados():{
-    host:
-    user:
-    password:
-    db:
-}
+engine = create_engine("mysql+pymysql://root:gure2408@localhost:3306/login")
+Base = declarative_base()
+
+class Usuario(Base):
