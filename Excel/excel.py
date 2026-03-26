0d85b1722f7b3fea38cbec0b2299704825675682
Gustavo Berg <gustavo2berg@gmail.com>
2026-03-26 14:41:39 -0300

    teste_db

--- Excel/excel.py
+++ Excel/excel.py
@@ -1,12 +1,21 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-tabela = pd.read_excel("Excel\Planilha de Teste Logística e Desafios.xlsx")
+tabela = pd.read_excel(r"Excel\Produtos.xlsx")
 print(tabela)
 
-def engine_dados():{
-    host:
-    user:
-    password:
-    db:
-}
+def engine_dados_teste():
+    #host = "127.0.0.1"
+    #user = "root"
+    #password = "gure2408"
+    #database = "login"
+
+    url = "sqlite:///banco_teste_logistica.db"
+
+    return create_engine(url)
+ 
+engine = engine_dados_teste()
+
+tabela.to_sql(name="produtos_logistica", con=engine, if_exists="replace", index=False)
+
+print("Sucesso! O arquivo 'banco_teste_logistica.db' foi criado na sua pasta.")
