729bc229376b32405294caf7ffaef88b777da63a
Gustavo Berg <gustavo2berg@gmail.com>
2026-03-31 15:18:39 -0300

    add EXCEL para CSV

--- app.py
+++ app.py
@@ -80,5 +80,36 @@ def converter_db_excel():
     except Exception as e:
         print(traceback.format_exc())
         return str(e), 500
+    
+@app.route("/converter-excel-csv", methods=["POST"])
+def converter_db_excel():
+    try:
+        arquivo_db = request.files["db"]
 
+       
+        if not arquivo_db.filename.endswith(".db"):
+            return "Arquivo inválido! Envie apenas .db", 400
+
+        caminho_db = "temp.db"
+        arquivo_db.save(caminho_db)
+
+        engine = create_engine(f"sqlite:///{caminho_db}")
+
+        tabelas = pd.read_sql(
+            "SELECT name FROM sqlite_master WHERE type='table';",
+            engine,
+        )
+
+        nome_tabela = tabelas.iloc[0, 0]
+
+        df = pd.read_sql(f"SELECT * FROM {nome_tabela}", engine)
+
+        nome_excel = "resultado.xlsx"
+        df.to_excel(nome_excel, index=False)
+
+        return send_file(nome_excel, as_attachment=True)
+
+    except Exception as e:
+        print(traceback.format_exc())
+        return str(e), 500
 
--- static/conection.js
+++ static/conection.js
@@ -36,12 +36,11 @@ async function enviarExcel() {
     const blob = await response.blob();
     const url = window.URL.createObjectURL(blob);
 
-    // Ajuste 3: "a" entre aspas e configuração do link de download
     const a = document.createElement("a");
     a.href = url;
-    a.download = "logistica_convertida.db"; // Sem isso, o navegador não sabe o nome do arquivo
+    a.download = "logistica_convertida.db";
 
-    document.body.appendChild(a); // É boa prática anexar antes de clicar
+    document.body.appendChild(a);
     a.click();
     a.remove();
 
@@ -82,6 +81,45 @@ async function enviarDB() {
     a.remove();
 
     console.log("DB convertido para Excel!");
+  } catch (error) {
+    alert("Erro: " + error.message);
+  }
+
+  button.innerText = "Convertendo...";
+  button.disabled = true;
+}
+
+async function enviarCSV() {
+  const input = document.getElementById("CSVinput");
+  const file = input.files[0];
+
+  if (!file) {
+    alert("Selecione um banco de dados!");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("CSV", file);
+
+  try {
+    const response = await fetch("https://automa-op-1.onrender.com/converter", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) throw new Error(`Erro: ${response.status}`);
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "convertido.xlsx";
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+
+    console.log("CSV convertido para Excel!");
   } catch (error) {
     alert("Erro: " + error.message);
   }
